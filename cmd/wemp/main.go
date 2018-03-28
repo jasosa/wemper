5f5871be02ac11be893be5a905ecf489fa282e85
Juan Antonio Sosa <juanantonio@typeform.com>
2018-03-28 12:44:21 +0200

    moved routing and creation of handlers to service struct

--- cmd/wemp/main.go
+++ cmd/wemp/main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"github.com/jasosa/wemper/pkg/service"
 	"log"
-	"net/http"
 )
 
 func main() {
-	router := mux.NewRouter()
-	getAllUsersHandlerWithHTTPError := service.HTTPErrorMiddleware(service.GetAllUsersHandler)
-	invitePersonsHandlerWithHTTPError := service.HTTPErrorMiddleware(service.InvitePersonHandler)
 
-	router.Handle("/persons",
-		service.LoggingMiddleware(getAllUsersHandlerWithHTTPError)).Methods("GET")
-	router.Handle("/persons/{id}/invitations/",
-		service.LoggingMiddleware(invitePersonsHandlerWithHTTPError)).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	svc := service.New()
+	server := svc.Server(":8080")
+	log.Fatal(server.ListenAndServe())
 }
